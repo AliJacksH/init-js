42ef5acf49f7b76f0db32970f569f1e8fa4b5f59
jayvnse <jason.santiago@hotmail.com>
2022-11-08 15:33:58 -0300

    mais introdução a js

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 // document.write("Bla bla bla bla");
 
-console.log("Ola");
+// console.log("Ola");
 
 // variável é um local de armazenamento de informações
 
-var nome = "Jackson";
-var idade = 29;
-var alturaEmCm = 184;
+// var nome = "Jackson";
+// var idade = 29;
+// var alturaEmCm = 184;
 
 
 
-document.write(nome);
-document.write(
-    alturaEmCm
-    );
+// document.write(nome);
+// document.write(
+//     alturaEmCm
+//     );
 
-console.log(idade);
+// console.log(idade);
+
+// DECLARE TRÊS VARIÁVEIS DIFERENTES, E MOSTRE-AS NO DOCUMENTO E NO CONSOLE
+
+//declaração de variáveis
+var fruta = "maça";
+var melhorComida = "batata";
+var filme = "matrix";
+
+document.write(fruta);
+console.log(melhorComida);
+
+var num1 = 10;
+var num2 = 15;
+var num3 = 2;
+var resultado = (num1 + num2 * num3);
+
+console.log(resultado);
+
+// soma +
+// subtração -
+// multiplicação *
+// divisão /
