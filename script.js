0b2b341990d61bcf33795bd268424c87aed65963
jayvnse <jason.santiago@hotmail.com>
2022-11-08 15:00:27 -0300

    inicio no js

---
+++ script.js
@@ -0,0 +1,18 @@
+// document.write("Bla bla bla bla");
+
+console.log("Ola");
+
+// variável é um local de armazenamento de informações
+
+var nome = "Jackson";
+var idade = 29;
+var alturaEmCm = 184;
+
+
+
+document.write(nome);
+document.write(
+    alturaEmCm
+    );
+
+console.log(idade);
