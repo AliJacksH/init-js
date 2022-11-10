ce297038ecbc280a46a1827daa0658d34f097be9
jayvnse <jason.santiago@hotmail.com>
2022-11-10 15:25:19 -0300

    mais js basics

--- script.js
+++ script.js
@@ -20,21 +20,47 @@
 // DECLARE TRÊS VARIÁVEIS DIFERENTES, E MOSTRE-AS NO DOCUMENTO E NO CONSOLE
 
 //declaração de variáveis
-var fruta = "maça";
-var melhorComida = "batata";
-var filme = "matrix";
+// var fruta = "maça";
+// var melhorComida = "batata";
+// var filme = "matrix";
 
-document.write(fruta);
-console.log(melhorComida);
+// document.write(fruta);
+// document.write(melhorComida);
+// document.write(filme);
 
-var num1 = 10;
-var num2 = 15;
-var num3 = 2;
-var resultado = (num1 + num2 * num3);
+// console.log(fruta);
+// console.log(melhorComida);
+// console.log(filme);
 
-console.log(resultado);
+// var num1 = 10;
+// var num2 = 15;
+// var num3 = 2;
+// var resultado = (num1 + num2) / num3;
+
+// console.log(resultado);
 
 // soma +
 // subtração -
 // multiplicação *
 // divisão /
+
+
+// NÚMERO em js sejam eles inteiros ou decimais são sempre do tipo number
+
+// TEXTO em js sejam eles uma letra/caractere ou uma palavra ou um paragrafo, etc serão sempre string
+
+//VERDADEIRO OU FALSO é sempre boolean
+
+
+// window.alert("o que eu quiser");
+// window.confirm("Você é legal?");
+// window.prompt("Qual o seu nome?");
+
+
+
+
+
+
+
+var nome = window.prompt("Qual o seu nome?");
+document.write("Bem vindo, " + nome);
