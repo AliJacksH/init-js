a8c4386c7d769641e8426e3fe6e25211f37cda6f
jayvnse <jason.santiago@hotmail.com>
2022-11-17 15:32:12 -0300

    conversao e coisinhas

--- script.js
+++ script.js
@@ -68,5 +68,58 @@ var idade = window.prompt("Qual sua idade?");
 
 
 
-document.write('Olá '+nome+'! que bom te ter aqui. Legal saber que você tem '+altura+'cm de altura e '+idade+' anos!');
+document.write('Olá ' + nome + '! Legal saber que você tem ' + altura + 'cm de altura e ' + idade + ' anos!</br>');
+document.write(`Olá ${nome}! Legal saber que você tem ${idade} anos e ${altura}cm de altura</br>`)
+document.write(`Seu nome tem ${nome.length} letras</br>`)
+document.write(`Seu nome em MAIÚSCULAS é: ${nome.toUpperCase()}`)
+
+
+//Escrever na página com o document.write()
+//escrever na janela com o window.alert()
+//perguntar com o window.prompt()
+// confirmar com o window.confirm()
+
+// Number.pasrseInt() para converter em NÚMERO INTEIRO
+// Number.pasrseFloat() para converter em NÚMERO REAL / DECIMAL
+// Number() para converter em número
+
+// var n1 = Number.parseInt(window.prompt("Informe o primeiro número:"))
+// var n2 = Number.parseInt(window.prompt("Informe o segundo número:"))
+
+// var resultado = n1 + n2
+
+// document.write("O resultado da soma é: " + resultado + ' ')
+
+
+// var n1 = Number(window.prompt("Informe o primeiro número:"));
+// var n2 = Number(window.prompt("Informe o segundo número:"));
+
+// var soma = n1 + n2;
+
+// document.write("O <strong>resultado</strong> da <strong>soma</strong> é: " + soma + '</br>');
+
+// var n3 = Number(window.prompt("Informe o primeiro número:"));
+// var n4 = Number(window.prompt("Informe o segundo número:"));
+
+// var subtracao = n3 - n4;
+
+// document.write("O resultado da <strong>subtração</strong> é: " + subtracao + '</br>');
+
+// var n5 = Number.parseFloat(window.prompt("Informe o primeiro número:"));
+// var n6 = Number.parseInt(window.prompt("Informe o segundo número:"));
+
+// var multiplicacao = n5 * n6;
+
+// document.write("O resultado da multiplicação é: " + multiplicacao + '</br>');
+
+
+// var n7 = Number.parseFloat(window.prompt("Informe o primeiro número:"));
+// var n8 = Number.parseInt(window.prompt("Informe o segundo número:"));
+
+// var divisao = n7 / n8;
+
+// document.write("O resultado da disisão é: " + divisao + '</br>');
+
+
+// Conversão de número para texto: String(n)   ou    n.toString()
 
